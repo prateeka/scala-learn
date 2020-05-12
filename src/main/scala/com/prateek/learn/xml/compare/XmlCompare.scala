6e6127586cc4a6a2817f922cdcd9bce78467239c
prateek <prateek.asthana@atscale.com>
2020-05-11 18:07:04 -0700

    intermediate versioListBuffer added to track the hierarchy

--- src/main/scala/com/prateek/learn/xml/compare/XmlCompare.scala
+++ src/main/scala/com/prateek/learn/xml/compare/XmlCompare.scala
@@ -5,21 +5,27 @@ import scala.xml.{Elem, MetaData, Node, Text}
 
 object XmlCompare extends App {
 
-  def apply[T <: String: ListBuffer](exp: Node, act: Node): Boolean = {
+  def apply(exp: Node, act: Node)(implicit lb: ListBuffer[String]): Boolean = {
+
+    def join = {
+      lb.toList.mkString(":")
+    }
+
     (exp, act) match {
-      case (e: Elem, a: Elem) =>
-        val a: T =
-          implicitly[ListBuffer[T]].append()
-        labelMatch(e, a) &&
-        attributesMatch(e, a) &&
-        e.child.forall(e1 =>
-          a.child.exists(a1 => {
-            println(s"elem: expected $e1 \n actual $a1")
-            apply(e1, a1)
-          })
-        )
+      case (e1: Elem, a1: Elem) =>
+        lb.append(e1.label)
+        val bool = labelMatch(e1, a1) &&
+          attributesMatch(e1, a1) &&
+          e1.child.forall(e1 =>
+            a1.child.exists(a1 => {
+              println(s"elem $join: expected $e1 \n actual $a1")
+              apply(e1, a1)
+            })
+          )
+        lb.remove(lb.size - 1)
+        bool
       case (Text(t1), Text(t2)) =>
-        println(s"text: expected $t1 \n actual $t2")
+        println(s"text $join: expected $t1 \n actual $t2")
         t1.trim == t2.trim
       case _ => false
     }
--- src/test/scala/com/prateek/learn/xml/compare/XmlCompareSpec.scala
+++ src/test/scala/com/prateek/learn/xml/compare/XmlCompareSpec.scala
@@ -9,57 +9,69 @@ class XmlCompareSpec extends AnyFreeSpec with Matchers {
     "elem comparison" - {
       "label comparison" - {
         "match" in {
+          import scala.collection.mutable.ListBuffer
           val exp = <Caption>Saturday, April 07 2001</Caption>
           val act = <Caption>Saturday, April 07 2001</Caption>
-          XmlCompare.apply(exp, act) should be(true)
+          XmlCompare.apply(exp, act)(new ListBuffer[String]) should be(true)
         }
 
         "does not match" in {
+          import scala.collection.mutable.ListBuffer
           val exp = <Caption>Saturday, April 07 2001</Caption>
           val act = <ABV>Saturday, April 07 2001</ABV>
-          XmlCompare.apply(exp, act) should be(false)
+          XmlCompare.apply(exp, act)(new ListBuffer[String]) should be(false)
         }
       }
 
       "attributes comparison when labels match" - {
         "match" in {
+          import scala.collection.mutable.ListBuffer
           val exp = <Caption attr="123">Saturday, April 07 2001</Caption>
           val act = <Caption attr="123">Saturday, April 07 2001</Caption>
-          XmlCompare.apply(exp, act) should be(true)
+          XmlCompare.apply(exp, act)(new ListBuffer[String]) should be(true)
         }
 
         "key does not match" in {
+          import scala.collection.mutable.ListBuffer
           val exp = <Caption attr="123">Saturday, April 07 2001</Caption>
           val act = <Caption cat="123">Saturday, April 07 2001</Caption>
-          XmlCompare.apply(exp, act) should be(false)
+          XmlCompare.apply(exp, act)(new ListBuffer[String]) should be(false)
         }
 
         "value does not match" in {
+          import scala.collection.mutable.ListBuffer
           val exp = <Caption attr="1231">Saturday, April 07 2001</Caption>
           val act = <Caption attr="123">Saturday, April 07 2001</Caption>
-          XmlCompare.apply(exp, act) should be(false)
+          XmlCompare.apply(exp, act)(new ListBuffer[String]) should be(false)
         }
       }
     }
 
     "text comparison" - {
       "match" in {
+        import scala.collection.mutable.ListBuffer
         val exp =
           <Caption>[DateCustom].[Retail445].[Reporting Day].&amp;[2001-04-07T00:00:00]</Caption>
         val act =
           <Caption>[DateCustom].[Retail445].[Reporting Day].&amp;[2001-04-07T00:00:00]</Caption>
-        XmlCompare.apply(exp.child.head, act.child.head) should be(true)
+        XmlCompare.apply(exp.child.head, act.child.head)(
+          new ListBuffer[String]
+        ) should be(true)
       }
 
       "does not match" in {
+        import scala.collection.mutable.ListBuffer
         val exp = <Caption>first</Caption>
         val act = <Caption>second</Caption>
-        XmlCompare.apply(exp.child.head, act.child.head) should be(false)
+        XmlCompare.apply(exp.child.head, act.child.head)(
+          new ListBuffer[String]
+        ) should be(false)
       }
     }
 
     "child comparison" - {
       "match" in {
+        import scala.collection.mutable.ListBuffer
         val exp =
           <Member Hierarchy="[DateCustom].[Retail445]">
             <UName>[DateCustom].[Retail445].[Reporting Day]</UName>
@@ -74,7 +86,7 @@ class XmlCompareSpec extends AnyFreeSpec with Matchers {
             <DisplayInfo>2</DisplayInfo>
           </Member>
 
-        XmlCompare.apply(exp, act) should be(true)
+        XmlCompare.apply(exp, act)(new ListBuffer[String]) should be(true)
       }
     }
   }
